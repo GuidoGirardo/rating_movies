bbdac7552b8da574e222e165bfe1d97804ecbbbe
guido girardo <guidogirardo@gmail.com>
2024-04-03 12:18:29 -0300

    design

--- app/src/main/java/com/damaris/rating_movies/view/screens/MoviesMain.kt
+++ app/src/main/java/com/damaris/rating_movies/view/screens/MoviesMain.kt
@@ -1,7 +1,9 @@
 package com.damaris.rating_movies.view.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,10 +12,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -25,15 +29,21 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.damaris.rating_movies.ViewModel.MovieViewModel
 import com.damaris.rating_movies.model.MovieEntity
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import com.damaris.rating_movies.view.navigation.AppScreens
+import com.damaris.rating_movies.view.ui.theme.background
+import com.damaris.rating_movies.view.ui.theme.borderColor
+import com.damaris.rating_movies.view.ui.theme.itemColor
+import com.damaris.rating_movies.view.ui.theme.textColor
 
 /* @Preview
 @Composable
@@ -53,9 +63,17 @@ fun MoviesMain(navController: NavController, viewModel: MovieViewModel = viewMod
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .background(background)
+            .padding(vertical = 16.dp)
     ) {
+
+        // primer elemento de la columna
         val movies by viewModel.movies.observeAsState(initial = emptyList())
-        LazyColumn {
+        LazyColumn(
+            modifier = Modifier
+                .weight(1f)
+                .padding(horizontal = 16.dp)
+        ) {
             items(movies) { movie ->
                 MovieItem(movie = movie) {
                     navController.navigate("${AppScreens.MoviesDetail.route}/${movie.id}")
@@ -63,57 +81,61 @@ fun MoviesMain(navController: NavController, viewModel: MovieViewModel = viewMod
             }
         }
 
-    }
-    Spacer(modifier = Modifier.height(20.dp))
-    Box(
-        modifier = Modifier.fillMaxWidth(),
-        contentAlignment = Alignment.BottomEnd
-    ) {
-        Row {
+        // espacio para separar los elementos
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // segundo elemento de la columna (fijado al bottom)
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+        ) {
             TextField(
-                modifier = Modifier.weight(1f),
+                modifier = Modifier
+                    .weight(1f)
+                    .clip(RoundedCornerShape(16.dp)),
                 value = movieName,
                 onValueChange = { movieName = it },
-                placeholder = { Text("Ingrese el nombre de la película") }
+                placeholder = { Text("Ingrese una película") }
             )
-
+            Spacer(modifier = Modifier.width(24.dp))
             Button(
                 onClick = {
                     viewModel.insertMovie(movieName)
-                }
+                },
+                modifier = Modifier.background(borderColor, RoundedCornerShape(16.dp)),
+                colors = ButtonDefaults.buttonColors(
+                    borderColor
+                )
             ) {
-                Text("enviar")
+                Text("enviar", color = itemColor, modifier = Modifier.background(borderColor, RoundedCornerShape(24.dp)), fontSize = 18.sp)
             }
         }
     }
-
 }
-
-
 
 @Composable
 fun MovieItem(movie: MovieEntity, onItemClick: () -> Unit) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
+            .height(50.dp)
+            .background(itemColor, RoundedCornerShape(16.dp))
             .clickable(onClick = onItemClick)
-            .padding(8.dp)
             .border(
                 width = 2.dp,
-                color = Color.Black,
+                color = borderColor,
                 shape = RoundedCornerShape(16.dp)
             )
     ) {
         Text(
             text = movie.title,
             style = MaterialTheme.typography.bodyMedium,
-            modifier = Modifier.padding(8.dp)
+            modifier = Modifier.padding(8.dp),
+            color = textColor
         )
     }
-}
-@Preview
-@Composable
-fun MovieItemPreview() {
-    val movie = MovieEntity(1, "Ejemplo de Película", 3)
-    MovieItem(movie = movie) {}
+    Spacer(modifier = Modifier.height(16.dp))
 }
--- app/src/main/java/com/damaris/rating_movies/view/ui/theme/Color.kt
+++ app/src/main/java/com/damaris/rating_movies/view/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// colores app
+val background = Color(0xFF0D1432)
+val itemColor = Color(0xFF18255F)
+val textColor = Color(0xFFE8E8E8)
+val borderColor = Color(0xFFFF5F8D)
